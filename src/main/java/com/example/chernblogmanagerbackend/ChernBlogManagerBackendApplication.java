a42c5632104391f33edee1b15158a46df1ce1bd2
Chern <781327871@qq.com>
2025-06-30 10:09:32 +0800

    initial

---
+++ src/main/java/com/example/chernblogmanagerbackend/ChernBlogManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chernblogmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChernBlogManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChernBlogManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chernblogmanagerbackend/ChernBlogManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chernblogmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChernBlogManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
